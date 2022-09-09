094bd08f45a72e51c38223fc1fc36e6afefcc128
charles ezebuike <ezebuike.charles0@gmail.com>
2022-09-09 17:11:26 +0000

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints numbers from 00 to 99
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = 0; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (!(tens == 9 && ones == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
