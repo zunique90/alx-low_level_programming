cfb1786ff39a14cfe0429c04bec0269b5955a836
charles ezebuike <ezebuike.charles0@gmail.com>
2022-09-09 13:34:55 +0000

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,16 +12,16 @@
 int main (void)
 {
 
-        char c:
+        char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
-	{
-		putchar(c);
-	}
+		{
+			putchar(c);
+		}
 	for (c = 'A' ; c <= 'Z' ; c++)
-	{
-		putchar(c);
-	}
-        putchar ('\n');
-        return (0);
+		{
+			putchar(c);
+		}
+	putchar ('\n');
+	return (0);
 }
