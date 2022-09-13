dd09853ae9e90521643917180284152fa7f8b34e
charles ezebuike <ezebuike.charles0@gmail.com>
2022-09-13 18:41:11 +0000

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the sum of all multiples of 3 or 5 below 1024
+ * followed by a new line
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum = sum + i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
