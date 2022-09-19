be670641c2b26c67d8afb384cda57d414f1c4c9b
charles ezebuike <ezebuike.charles0@gmail.com>
2022-09-19 11:21:10 +0000

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,20 +3,17 @@
 /**
  * _puts - prints a string followed by a new line
  *
- * @str: pointer to string we are to print
+ * @str: string we are to print
  *
  * Return: nothing
  */
 
 void _puts(char *str)
 {
-	int i;
 
-	i = 0;
-	while (*(str + 1) != '\0')
-	{
-		_putchar(*(str + i));
-		i++;
-	}
+	while (*str)
+		_putchar(*str++);
+
+
 	_putchar('\n');
 }
