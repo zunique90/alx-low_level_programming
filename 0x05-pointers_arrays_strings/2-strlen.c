5eb15b3eaf80c3ecc90725e86c4950564183b398
charles ezebuike <ezebuike.charles0@gmail.com>
2022-09-19 11:01:28 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s[l] != '\0')
+	{
+		l++
+	}
+
+	return (l);
+}
