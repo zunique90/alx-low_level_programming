a79d895e17859f929d25b4c1a1d103fb8c1f45de
charlesezebuike <ezebuike.charles0@gmail.com>
2022-09-19 13:49:33 +0000

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int j = 0;
 
-	while (src[i])
+	while (*(src + i) != '\0')
 	{
-		dest[i] = src[i];
 		i++;
 	}
 
-	return (dest);
+	i++;
+	while (j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+
+	return (&*dest);
 }
