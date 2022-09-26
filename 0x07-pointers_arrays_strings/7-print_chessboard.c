1fd79aa2ee1b0934443c116e258e53257e31ca70
charles ezebuike <ezebuike.charles0@gmail.com>
2022-09-26 18:57:43 +0000

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: rows
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
