4e843fc8cd8ab079a2f6555fd6be0d76e5f2693e
charlesezebuike <ezebuike.charles0@gmail.com>
2022-10-03 10:22:16 +0000

    task 2 fix for memory size

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,9 +10,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int l1 = 0;
+	int l = 0;
 	int l2 = 0;
-	int l3 = 0;
 	int i;
 	char *s3;
 
@@ -22,22 +21,19 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i]; i++)
-		l1++;
+	for (i = 0; s1[i] || s2[i]; i++)
+		l++;
 
-	for (i = 0; s2[i]; i++)
-		l2++;
-
-	s3 = malloc((l1 + l2) * sizeof(char));
+	s3 = malloc(l * sizeof(char));
 
 	if (s3 == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
-		s3[l3++] = s1[i];
+		s3[l2++] = s1[i];
 
 	for (i = 0; s2[i]; i++)
-		s3[l3++] = s2[i];
+		s3[l2++] = s2[i];
 
 	return (s3);
 }
