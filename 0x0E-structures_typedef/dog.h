976472c594d838b71f0a3747c5c691e54a5bce3f
charlesezebuike <ezebuike.charles0@gmail.com>
2022-10-10 10:25:00 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type
+ * @name: first member of the structure
+ * @age: second member of the structure
+ * @owner: third member of the structure
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
+
+#endif /* DOG_H */
