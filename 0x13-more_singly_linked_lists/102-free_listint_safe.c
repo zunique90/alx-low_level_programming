9545a6b4a84d526153277f43e667072b9dc3d071
charles ezebuike <ezebuike.charles0@gmail.com>
2022-10-26 04:52:43 +0000

    corrections to return type (tast13)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * find_listint_loop - finds the loop in a linked list,
+ * loop_listint_count - finds the loop in a linked list,
  * and returns the number of nodes that are unique
  * @head: pointer to the head node of the list
  *
  * Return: number of nodes that are unique
  * or 0 if there is no loop
  */
-size_t *find_listint_loop(listint_t *head)
+size_t loop_listint_count(listint_t *head)
 {
 	listint_t *slow, *fast;
 	size_t nodes = 1;
@@ -41,7 +41,7 @@ size_t *find_listint_loop(listint_t *head)
 		slow = slow->next;
 		fast = (fast->next)->next;
 	}
-	return (NULL);
+	return (0);
 }
 /**
  * free_listint_safe - frees a listint_t list that has a loop
@@ -53,7 +53,7 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *temp;
 	size_t size, i;
 
-	size = find_listint_loop(*h);
+	size = loop_listint_count(*h);
 
 	if (size == 0)
 	{
