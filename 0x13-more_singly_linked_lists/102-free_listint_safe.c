dd403fdf7590a2e729f7f6cdf71f9741bc52ab4f
charles ezebuike <ezebuike.charles0@gmail.com>
2022-10-26 04:31:54 +0000

    added find loop function to task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,29 +1,79 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_listint_safe - frees a listint_t list.
+ * find_listint_loop - finds the loop in a linked list,
+ * and returns the number of nodes that are unique
+ * @head: pointer to the head node of the list
+ *
+ * Return: number of nodes that are unique
+ * or 0 if there is no loop
+ */
+size_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+				nodes++;
+			}
+			slow = slow->next;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				nodes++;
+			}
+			return (nodes);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+	return (NULL);
+}
+/**
+ * free_listint_safe - frees a listint_t list that has a loop
  * @h: pointer to the head node of list to be free'd
  * Return: size of the list that was free'd
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *copy, *temp;
-	size_t size = 0;
+	listint_t *temp;
+	size_t size, i;
 
-	temp = *h;
-	if (h == NULL)
-		return (0);
+	size = find_listint_loop(*h);
 
-	while (temp != NULL)
+	if (size == 0)
 	{
-		size++;
-		copy = temp;
-		temp = temp->next;
-		free(copy);
-		if (copy < temp)
-			break;
-
+		for (; h != NULL && *h != NULL; size++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
 		*h = NULL;
 	}
+	h = NULL;
 	return (size);
 }
