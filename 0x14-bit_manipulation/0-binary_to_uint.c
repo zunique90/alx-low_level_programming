4624d99ddfa8c07d54b6d3258272796b0710ceb6
charles ezebuike <ezebuike.charles0@gmail.com>
2022-10-27 14:41:18 +0000

    task 0.0!

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,5 @@
 #include "main.h"
 /**
- * _pow - raise to power num
- * @num: number
- * @n: number of times
- * Return: power to number
- */
-unsigned int _pow(unsigned int num, unsigned int n)
-{
-	unsigned int sum, i;
-
-	sum = 1;
-	for (i = 0; i < n; i++)
-	{
-		sum = sum * num;
-
-	}
-	return (sum);
-}
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: points to a string of 0 and 1 chars
  * Return: the converted number
@@ -25,29 +7,22 @@ unsigned int _pow(unsigned int num, unsigned int n)
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-	unsigned int dec, i, k;
+	unsigned int dec = 0;
+	int l, bin;
 
 	if (b == NULL)
 		return (0);
-	for (k = 0; b[k] != '\0'; k++)
+
+	for (l = 0; b[l] != '\0'; l++)
+		;
+
+	for (l--, bin = 1; l >= 0; l--, bin *= 2)
 	{
-		if (((b[k]) != '0') && ((b[k]) != '1') && ((b[k]) != '\0'))
+		if (b[l] != '0' && b[l] != '1')
 			return (0);
-	}
-	k = k - 1;
-	dec = 0;
-	i = 0;
-	while (b[i] != '\0')
-	{
-		if ((b[k - i]) == '1')
-		{
-			if (i == 0)
-				dec += 1;
-			else
-				dec += _pow(2, i);
-		}
-		i++;
+
+		if (b[l] & 1)
+			dec += bin;
 	}
 	return (dec);
 }
