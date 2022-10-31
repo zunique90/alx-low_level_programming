09f186dc5377807c23097463c7185f25fbb340fa
charlesezebuike <ezebuike.charles0@gmail.com>
2022-10-31 20:05:39 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		op = open(filename, O_CREAT, 0600);
+		if (op == -1)
+			return (-1);
+		return (1);
+	}
+
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i];)
