53ea329e76a755fdfb70c2de267c7db0522f21ab
charles ezebuike <ezebuike.charles0@gmail.com>
2022-12-08 14:01:31 +0000

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,15 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *tmp;
-	size_t n = 0;
+	size_t nodes = 0;
 
-	tmp = h;
-	while (tmp != 0)
+	while (h)
 	{
 		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-		n++;
+		h = h->next;
+		nodes++;
 	}
-	return (n);
+	return (nodes);
 }
