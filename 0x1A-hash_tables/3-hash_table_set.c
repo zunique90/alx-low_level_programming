5a7ce3fa35a4873ffdf5ee45316e9c0829ce7c33
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-06 05:54:58 +0000

    task 3 check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->value = strdup(value);
 	new_node->key = strdup(key);
-	new_node->next = NULL;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
